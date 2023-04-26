977aa7d7e51887cf3046d4103b93f158823ff97b
Said Alshamsi <eng.saidj@gmail.com>
2023-04-26 15:53:30 +0400

    'test'

---
+++ src/main/java/Models/Teacher.java
@@ -0,0 +1,27 @@
+package Models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Data
+@Table(name = "Teacher_Table")
+public class Teacher {
+
+    @Id
+    Integer id;
+
+    @Column(name = "teacher_name",  nullable = false)
+    String name;
+
+    @Column
+    String group_name;
+}
---
+++ src/main/java/Models/TeacherDetail.java
@@ -0,0 +1,31 @@
+package Models;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Data
+@Table(name = "Teacher_Detail_Table")
+public class TeacherDetail {
+
+
+    @Id
+    Integer id;
+
+    @Column(name = "teacher_phone_number")
+    String phoneNumber;
+
+    @Column(name = "teacher_address")
+    String address;
+
+    @OneToOne
+    Teacher teacher;
+
+
+}
